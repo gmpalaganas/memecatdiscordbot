1d2ef0777e7527dd73adcf4ec08a11bbfdc32225
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2016-06-13 06:50:46 +0800

    "Add new commands"

--- src/memecatbot.js
+++ src/memecatbot.js
@@ -24,10 +24,22 @@ var ouchStrings = [
     'itai'
 ];
 
-
 bot.on("message", (msg) => {
     if (ouchStrings.indexOf(msg.content) > -1){
-       bot.reply(msg, " on a scale from 1-10, how would you rate your pain?");
+        bot.reply(msg, " on a scale from 1-10, how would you rate your pain?");
+        console.log("Received: " + msg.content);
+    }
+    else if(msg.content === callPrefix + " who killed you?" || msg.content === callPrefix + " sinong pumatay sayo?"){
+        bot.reply(msg,"Albei Keith Capito Tolete");
+        console.log("Received: " + msg.content);
+    }
+    else if(msg.content === callPrefix){
+        bot.reply(msg, "Nyaaa~");
+        console.log("Received: " + msg.content);
+    }
+    else if(msg.content === "(╯°□°）╯︵ ┻━┻"){
+        bot.reply(msg, " please respect tables ┬──┬ ノ( ゜-゜ノ)");
+        console.log("Received: " + msg.content);
     }
 });
 
