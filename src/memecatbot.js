be86ff3d16bc930f42b3ef04e6ace9e0cb08155d
Genesis Ian Palaganas <palaganas.genesis@gmail.com>
2016-06-13 08:24:34 +0800

    Add xkcd command

--- src/memecatbot.js
+++ src/memecatbot.js
@@ -1,4 +1,6 @@
 var Discord = require('discord.js');
+var xkcd = require('xkcd-imgs');
+
 var authDetails = require('../res/auth.json');
     
 var bot = new Discord.Client();
@@ -27,21 +29,23 @@ bot.on("message", (msg) => {
     if (ouchStrings.indexOf(msg.content) > -1){
         bot.reply(msg, " on a scale from 1-10, how would you rate your pain?");
         console.log("Received: " + msg.content);
-    }
-    else if(msg.content === callPrefix + " who killed you?" || msg.content === callPrefix + " sinong pumatay sayo?"){
+    } else if(msg.content === callPrefix + " who killed you?" || msg.content === callPrefix + " sinong pumatay sayo?"){
         bot.reply(msg,"Albei Keith Capito Tolete");
         console.log("Received: " + msg.content);
-    }
-    else if(msg.content === callPrefix){
+    } else if(msg.content === callPrefix){
         bot.reply(msg, "Nyaaa~");
         console.log("Received: " + msg.content);
-    }
-    else if(msg.content === "(╯°□°）╯︵ ┻━┻"){
+    } else if(msg.content === "(╯°□°）╯︵ ┻━┻"){
         bot.reply(msg, " please respect tables ┬──┬ ノ( ゜-゜ノ)");
         console.log("Received: " + msg.content);
-    }
-    else if(msg.content === callPrefix + " who created you?"){
+    } else if(msg.content === callPrefix + " who created you?"){
         bot.reply(msg, " the legendary Genesis-sama created me");
+    } else if(msg.content === callPrefix + " xkcd" ){
+            xkcd.img(function(img){
+                var msg = "XKCD Image: " + img.title;
+                msg += "\n" + img.url;
+                bot.sendMessage(msg.channel, msg);
+            });
     }
 });
 
